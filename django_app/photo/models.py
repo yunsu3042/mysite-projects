9e6c274551015e9cb10fba0a20ee2776c0b71f16
yunsoo3042 <yunsoo3042@gmail.com>
2016-10-25 17:29:27 +0900

    photo의 models에 like_users, dislike_users 필드 추가 및 중간자 모델 구현, 해당내용 migrations생성

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -15,3 +15,16 @@ class Photo(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=80, blank=True)
     img = models.ImageField(upload_to='photo')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,through='PhotoLike',related_name='photo_set_like_users')
+    dislike_users = models.ManyToManyField(settings.AUTH_USER_MODEL,through='PhotoDIsLike',related_name='photo_set_dislike_users')
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PhotoDisLike(models.Model):
+    photo = models.ForeignKey(Photo)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
