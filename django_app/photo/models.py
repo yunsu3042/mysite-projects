5af8b54f261545a5d7de2a30d9aabcee2675bc41
yunsoo3042 <yunsoo3042@gmail.com>
2016-10-25 16:32:34 +0900

    photo에 모델추가'

--- django_app/photo/models.py
+++ django_app/photo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    description = models.CharField(max_length=80)
+
+
+class Photo(models.Model):
+    album = models.ForeignKey(Album)
+    owner = models.ForeignKey()
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, blank=True)
+    img = models.ImageField(uplaoad_to='photo')
